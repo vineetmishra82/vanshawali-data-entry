38c5bcd9d20ca04819d04b64043049edcad06351
Vineet Mishra <vineet.mishra82@gmail.com>
2022-08-17 20:37:46 +0530

    deply

--- Downloads/UNIGE_REST_APIs-main/src/main/java/com/pts/unige/Configuration/CorsConfiguration.java
+++ Downloads/UNIGE_REST_APIs-main/src/main/java/com/pts/unige/Configuration/CorsConfiguration.java
@@ -14,7 +14,8 @@ public class CorsConfiguration
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-            	 registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            	registry.addMapping("/**")
+                .allowedMethods("*");
             	
             	
             }
